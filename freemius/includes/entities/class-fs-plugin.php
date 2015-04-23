e1677ec4a7c7ca287adb568891fe05a68b5b145a
vovafeldman <vovafeldman@vovas-mbp.home>
2015-04-22 20:48:00 -0400

    Added method to check if a plugin is an add-on.

--- freemius/includes/entities/class-fs-plugin.php
+++ freemius/includes/entities/class-fs-plugin.php
@@ -58,6 +58,19 @@
 				$this->info = new FS_Plugin_Info($plugin->info);
 		}
 
+		/**
+		 * Check if plugin is an add-on (has parent).
+		 *
+		 * @author Vova Feldman (@svovaf)
+		 * @since  1.0.6
+		 *
+		 * @return bool
+		 */
+		function is_addon()
+		{
+			return isset($this->parent_plugin_id) && is_numeric($this->parent_plugin_id);
+		}
+
 		static function get_type()
 		{
 			return 'plugin';
