f6021eade5f23100ae24c5111d94e1e08735127b
vovafeldman <vovafeldman@vovas-mbp.home>
2015-05-24 18:29:20 -0400

    Added checkout iframe disclaimer notice + started to integrate the option to checkout without being connected to Freemius (when skipped).

--- freemius/templates/checkout.php
+++ freemius/templates/checkout.php
@@ -10,23 +10,21 @@
 	wp_enqueue_script('json2');
 	fs_enqueue_local_script('postmessage', 'nojquery.ba-postmessage.min.js');
 	fs_enqueue_local_script('fs-postmessage', 'postmessage.js');
+	fs_enqueue_local_style( 'fs_common', '/admin/common.css' );
 
 	$slug = $VARS['slug'];
 	$fs = fs($slug);
 
 	$timestamp = time();
 
-//	$plan_id = fs_request_get('plan_id');
-//	$billing_cycle = fs_request_get('billing_cycle', WP_FS__PERIOD_MONTHLY);
-//	$licenses = fs_request_get('licenses', 1);
-//	$pricing_id = fs_request_get('pricing_id', false);
-
 	// Get site context secure params.
-	$context_params = FS_Security::instance()->get_context_params(
-		$fs->get_site(),
-		$timestamp,
-		'checkout'
-	);
+	$context_params = !$fs->is_registered() ?
+		array() :
+		FS_Security::instance()->get_context_params(
+			$fs->get_site(),
+			$timestamp,
+			'checkout'
+		);
 
 	if ($fs->is_payments_sandbox())
 		// Append plugin secure token for sandbox mode authentication.)
@@ -36,23 +34,25 @@
 			'checkout'
 		);
 
-//	if (false !== $pricing_id)
-//		$context_params['pricing_id'] = $pricing_id;
-
 	$query_params = array_merge($context_params, $_GET, array(
-		// Adds plan ID.
-//		'plan_id' => $plan_id,
-		// Licenses number.
-//		'licenses' => $licenses,
-		// Billing cycle.
-//		'billing_cycle' => $billing_cycle,
 		// Current plugin version.
 		'plugin_version' => $fs->get_plugin_version(),
+		'return_url'     => wp_nonce_url($fs->_get_admin_page_url(
+			'account',
+			array(
+				'fs_action' => $slug . '_sync_license',
+				'plugin_id' => isset($_GET['plugin_id']) ? $_GET['plugin_id'] : $fs->get_id()
+			)
+		), $slug . '_sync_license'),
 		// Admin CSS URL for style/design competability.
-		'wp_admin_css' => get_bloginfo('wpurl') . "/wp-admin/load-styles.php?c=1&load=buttons,wp-admin,dashicons",
+		'wp_admin_css'   => get_bloginfo('wpurl') . "/wp-admin/load-styles.php?c=1&load=buttons,wp-admin,dashicons",
 	));
 ?>
-<div id="fs_contact" class="wrap" style="margin: 0 0 -65px -20px;">
+<div class="fs-secure-notice">
+	<i class="dashicons dashicons-lock"></i>
+	<span><b>Secure HTTPS Checkout</b> - PCI compliant, running via iframe from external domain</span>
+</div>
+<div id="fs_contact" class="wrap" style="margin: 40px 0 -65px -20px;">
 	<div id="iframe"></div>
 	<script type="text/javascript">
 		(function($) {
@@ -77,6 +77,38 @@
 						iframe_height = h;
 						$("#iframe iframe").height(iframe_height + 'px');
 					}
+				});
+				FS.PostMessage.receive('get_context', function (){
+					// If the user didn't connect his account with Freemius,
+					// once he accepts the Terms of Service and Privacy Policy,
+					// and then click the purchase button, the context information
+					// of the user will be shared with Freemius in order to complete the
+					// purchase workflow and activate the license for the right user.
+					<?php $current_user = wp_get_current_user() ?>
+					FS.PostMessage.post('user_context', {
+						user: {
+							firstname: '<?php echo $current_user->user_firstname ?>',
+							lastname: '<?php echo $current_user->user_lastname ?>',
+							nickname: '<?php echo $current_user-> user_nicename ?>',
+							email: '<?php echo $current_user->user_email ?>'
+						},
+						plugin: {
+							id: '<?php echo $fs->get_id() ?>',
+							slug: '<?php echo $slug ?>',
+							public_key: '<?php echo $fs->get_public_key() ?>',
+							version: '<?php echo $fs->get_plugin_version() ?>'
+						},
+						site: {
+							name: '<?php echo get_bloginfo('name') ?>',
+							version: '<?php echo get_bloginfo('version') ?>',
+							language: '<?php echo get_bloginfo('language') ?>',
+							charset: '<?php get_bloginfo('charset') ?>',
+							account_url: '<?php echo wp_nonce_url($fs->_get_admin_page_url(
+									'account',
+									array('fs_action' => 'sync_user')
+							), 'sync_user') ?>'
+						}
+					});
 				});
 			});
 		})(jQuery);
