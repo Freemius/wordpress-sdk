95111f6f3b168a922433198f245ea92645274099
Vova Feldman <vova@freemius.com>
2016-01-14 18:35:19 -0500

    [connect] [opt-in] [bug-fix] Since only one SDK version is loaded, WP_FS__DIR_IMG will always point to the image assets of that SDK. This optimization make sure to select the plugin's icon from the plugin's SDK image assets (and not just from the newest SDK).

--- templates/plugin-icon.php
+++ templates/plugin-icon.php
@@ -8,10 +8,23 @@
 	$slug = $VARS['slug'];
 	$fs   = freemius( $slug );
 
-	$icons = glob( fs_normalize_path( WP_FS__DIR_IMG . '/icon.*' ) );
+	global $fs_active_plugins;
+
+	$img_dir = WP_FS__DIR_IMG;
+
+	if ( 1 < count( $fs_active_plugins->plugins ) ) {
+		foreach ( $fs_active_plugins->plugins as $sdk_path => &$data ) {
+			if ( $data->plugin_path == $fs->get_plugin_basename() ) {
+				$img_dir = WP_PLUGIN_DIR . '/' . $sdk_path . '/assets/img';
+				break;
+			}
+		}
+	}
+
+	$icons = glob( fs_normalize_path( $img_dir . '/icon.*' ) );
 	if ( ! is_array( $icons ) || 0 === count( $icons ) ) {
 		$icon_found = false;
-		$local_path = fs_normalize_path( WP_FS__DIR_IMG . '/icon.png' );
+		$local_path = fs_normalize_path( $img_dir . '/icon.png' );
 
 		if ( WP_FS__IS_LOCALHOST && $fs->is_org_repo_compliant() ) {
 			/**
@@ -35,7 +48,7 @@
 			foreach ( $suffixes as $s ) {
 				$headers = get_headers( $base_url . $s );
 				if ( strpos( $headers[0], '200' ) ) {
-					$local_path = fs_normalize_path( WP_FS__DIR_IMG . '/icon.' . substr( $s, strpos( $s, '.' ) + 1 ) );
+					$local_path = fs_normalize_path( $img_dir . '/icon.' . substr( $s, strpos( $s, '.' ) + 1 ) );
 					fs_download_image( $base_url . $s, $local_path );
 					$icon_found = true;
 					break;
@@ -45,13 +58,13 @@
 
 		if ( ! $icon_found ) {
 			// No icons found, fallback to default icon.
-			copy( fs_normalize_path( WP_FS__DIR_IMG . '/plugin-icon.png' ), $local_path );
+			copy( fs_normalize_path( $img_dir . '/plugin-icon.png' ), $local_path );
 		}
 
 		$icons = array( $local_path );
 	}
 
-	$relative_url = fs_img_url( substr( $icons[0], strlen( fs_normalize_path( WP_FS__DIR_IMG ) ) ) );
+	$relative_url = fs_img_url( substr( $icons[0], strlen( fs_normalize_path( $img_dir ) ) ), $img_dir );
 ?>
 <div class="fs-plugin-icon">
 	<img src="<?php echo $relative_url ?>"/>
