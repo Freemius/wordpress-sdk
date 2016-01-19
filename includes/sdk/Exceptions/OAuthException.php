bed2d3f8fde1c0e82508bf00209bbdfaaeee0e8c
Vova Feldman <vova@freemius.com>
2016-01-18 20:07:18 -0500

    [security] [php-sdk] Added logic to check if Freemius parent exception exist, to prevent PHP error (with real path to the file) on direct access.

--- includes/sdk/Exceptions/OAuthException.php
+++ includes/sdk/Exceptions/OAuthException.php
@@ -1,4 +1,8 @@
 <?php
+	if ( ! class_exists( 'Freemius_Exception' ) ) {
+		exit;
+	}
+
     class Freemius_OAuthException extends Freemius_Exception
     {
         public function __construct($pResult)
