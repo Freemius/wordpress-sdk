b451c4fa4e9045adfafe0d60a4c74ed48e9348b1
Vova Feldman <vova@freemius.com>
2016-02-27 16:11:43 -0500

    [admin-notice] [template] Improved the admin notice to work with subset of the arguments.

--- templates/admin-notice.php
+++ templates/admin-notice.php
@@ -10,31 +10,35 @@
 		exit;
 	}
 ?>
-<div data-id="<?php echo $VARS['id'] ?>" data-slug="<?php echo $VARS['slug'] ?>" class="<?php
-	switch ( $VARS['type'] ) {
-		case 'error':
-			echo 'error form-invalid';
-			break;
-		case 'promotion':
-			echo 'updated promotion';
-			break;
-		case 'update':
+<div<?php if ( ! empty( $VARS['id'] ) ) : ?> data-id="<?php echo $VARS['id'] ?>"<?php endif ?><?php if ( ! empty( $VARS['slug'] ) ) : ?> data-slug="<?php echo $VARS['slug'] ?>"<?php endif ?>
+	class="<?php
+		switch ( $VARS['type'] ) {
+			case 'error':
+				echo 'error form-invalid';
+				break;
+			case 'promotion':
+				echo 'updated promotion';
+				break;
+			case 'update':
 //			echo 'update-nag update';
 //			break;
-		case 'success':
-		default:
-			echo 'updated success';
-			break;
-	}
-?> fs-notice<?php if ( $VARS['sticky'] ) {
-	echo ' fs-sticky';
-} ?><?php if ( ! empty( $VARS['plugin'] ) ) {
-	echo ' fs-has-title';
-} ?>"><?php if ( ! empty( $VARS['plugin'] ) ) : ?><label
-		class="fs-plugin-title"><?php echo $VARS['plugin'] ?></label><?php endif ?><?php if ( $VARS['sticky'] ) : ?>
+			case 'success':
+			default:
+				echo 'updated success';
+				break;
+		}
+	?> fs-notice<?php if ( ! empty( $VARS['sticky'] ) ) {
+		echo ' fs-sticky';
+	} ?><?php if ( ! empty( $VARS['plugin'] ) ) {
+		echo ' fs-has-title';
+	} ?>"><?php if ( ! empty( $VARS['plugin'] ) ) : ?>
+		<label class="fs-plugin-title"><?php echo $VARS['plugin'] ?></label>
+	<?php endif ?>
+	<?php if ( ! empty( $VARS['sticky'] ) ) : ?>
 		<div class="fs-close"><i class="dashicons dashicons-no"
 		                         title="<?php _efs( 'dismiss' ) ?>"></i> <span><?php _efs( 'dismiss' ) ?></span>
-		</div><?php endif ?>
+		</div>
+	<?php endif ?>
 	<div class="fs-notice-body">
 		<?php if ( ! empty( $VARS['title'] ) ) : ?><b><?php echo $VARS['title'] ?></b> <?php endif ?>
 		<?php echo $VARS['message'] ?>
