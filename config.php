fbd81e1f1fde82ccab544addea8d0ce708ec7c19
Vova Feldman <vova@freemius.com>
2016-02-27 15:47:09 -0500

    [config] Reorganized all dev mode constants to enable external override of the values from external file instead of modifying the config file itself and mistakenly deploying a version with dev/debug arguments turned on.

--- config.php
+++ config.php
@@ -60,6 +60,7 @@
 		define( 'WP_FS__SKIP_EMAIL_ACTIVATION', false );
 	}
 
+
 	/**
 	 * Directories
 	 */
@@ -76,16 +77,34 @@
 	/**
 	 * Domain / URL / Address
 	 */
-	define( 'WP_FS__TESTING_DOMAIN', 'fswp:8080' );
 	define( 'WP_FS__DOMAIN_PRODUCTION', 'wp.freemius.com' );
-	define( 'WP_FS__DOMAIN_LOCALHOST', 'wp.freemius' );
-	define( 'WP_FS__ADDRESS_LOCALHOST', 'http://' . WP_FS__DOMAIN_LOCALHOST . ':8080' );
 	define( 'WP_FS__ADDRESS_PRODUCTION', 'https://' . WP_FS__DOMAIN_PRODUCTION );
+
+	if ( ! defined( 'WP_FS__DOMAIN_LOCALHOST' ) ) {
+		define( 'WP_FS__DOMAIN_LOCALHOST', 'wp.freemius' );
+	}
+	if ( ! defined( 'WP_FS__ADDRESS_LOCALHOST' ) ) {
+		define( 'WP_FS__ADDRESS_LOCALHOST', 'http://' . WP_FS__DOMAIN_LOCALHOST . ':8080' );
+	}
+
+	if ( ! defined( 'WP_FS__TESTING_DOMAIN' ) ) {
+		define( 'WP_FS__TESTING_DOMAIN', 'fswp' );
+	}
+
+	if ( ! defined( 'WP_FS__API_ADDRESS_LOCALHOST' ) ) {
+		define( 'WP_FS__API_ADDRESS_LOCALHOST', 'http://api.freemius:8080' );
+	}
+	if ( ! defined( 'WP_FS__API_SANDBOX_ADDRESS_LOCALHOST' ) ) {
+		define( 'WP_FS__API_SANDBOX_ADDRESS_LOCALHOST', 'http://sandbox-api.freemius:8080' );
+	}
 
 	define( 'WP_FS__IS_HTTP_REQUEST', isset( $_SERVER['HTTP_HOST'] ) );
 	define( 'WP_FS__REMOTE_ADDR', fs_get_ip() );
 
-	define( 'WP_FS__IS_PRODUCTION_MODE', ! defined( 'WP_FS__DEV_MODE' ) || ! WP_FS__DEV_MODE || ( WP_FS__TESTING_DOMAIN !== $_SERVER['HTTP_HOST'] ) );
+	if ( ! defined( 'WP_FS__IS_PRODUCTION_MODE' ) ) {
+		// By default, run with Freemius production servers.
+		define( 'WP_FS__IS_PRODUCTION_MODE', true );
+	}
 
 	define( 'WP_FS__ADDRESS', ( WP_FS__IS_PRODUCTION_MODE ? WP_FS__ADDRESS_PRODUCTION : WP_FS__ADDRESS_LOCALHOST ) );
 
@@ -104,8 +123,8 @@
 
 	// Set API address for local testing.
 	if ( ! WP_FS__IS_PRODUCTION_MODE ) {
-		define( 'FS_API__ADDRESS', 'http://api.freemius:8080' );
-		define( 'FS_API__SANDBOX_ADDRESS', 'http://sandbox-api.freemius:8080' );
+		define( 'FS_API__ADDRESS', WP_FS__API_ADDRESS_LOCALHOST );
+		define( 'FS_API__SANDBOX_ADDRESS', WP_FS__API_SANDBOX_ADDRESS_LOCALHOST );
 	}
 
 	define( 'WP_FS___OPTION_PREFIX', 'fs' . ( WP_FS__IS_PRODUCTION_MODE ? '' : '_dbg' ) . '_' );
@@ -119,8 +138,8 @@
 	define( 'WP_FS__OPTIONS_OPTION_NAME', WP_FS___OPTION_PREFIX . 'options' );
 
 	define( 'WP_FS__IS_HTTPS', ( WP_FS__IS_HTTP_REQUEST &&
-		                           // Checks if CloudFlare's HTTPS (Flexible SSL support)
-		                           isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && 'https' === strtolower( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) ) ||
+	                             // Checks if CloudFlare's HTTPS (Flexible SSL support)
+	                             isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && 'https' === strtolower( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) ) ||
 	                           // Check if HTTPS request.
 	                           ( isset( $_SERVER['HTTPS'] ) && 'on' == $_SERVER['HTTPS'] ) ||
 	                           ( isset( $_SERVER['SERVER_PORT'] ) && 443 == $_SERVER['SERVER_PORT'] )
@@ -154,9 +173,16 @@
 	/**
 	 * Debugging
 	 */
-	define( 'WP_FS__DEBUG_SDK', WP_FS__DEV_MODE && ! empty( $_GET['fs_dbg'] ) );
+	if ( ! defined( 'WP_FS__DEBUG_SDK' ) ) {
+		$debug_mode = get_option( 'fs_debug_mode' );
+		define( 'WP_FS__DEBUG_SDK', is_numeric( $debug_mode ) ? ( 0 < $debug_mode ) : WP_FS__DEV_MODE );
+	}
+
 	define( 'WP_FS__ECHO_DEBUG_SDK', WP_FS__DEV_MODE && ! empty( $_GET['fs_dbg_echo'] ) );
 	define( 'WP_FS__LOG_DATETIME_FORMAT', 'Y-n-d H:i:s' );
+	if ( ! defined( 'FS_API__LOGGER_ON' ) ) {
+		define( 'FS_API__LOGGER_ON', WP_FS__DEBUG_SDK );
+	}
 
 	if ( WP_FS__ECHO_DEBUG_SDK ) {
 		error_reporting( E_ALL );
