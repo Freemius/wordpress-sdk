4dd09ba93b7a3104c1b07dd1fe6262f26f478936
Vova Feldman <vova@freemius.com>
2016-07-25 15:42:13 -0400

    [powered-by] Added more details to the powered by to get a better understanding on the referer.

--- templates/account.php
+++ templates/account.php
@@ -622,4 +622,12 @@
 	</div>
 	</div>
 	</div>
-<?php fs_require_template( 'powered-by.php' ) ?>
+<?php
+	$params = array(
+		'page'           => 'account',
+		'module_id'      => $fs->get_id(),
+		'module_slug'    => $slug,
+		'module_version' => $fs->get_plugin_version(),
+	);
+	fs_require_template( 'powered-by.php', $params );
+?>
--- templates/add-ons.php
+++ templates/add-ons.php
@@ -141,4 +141,12 @@
 			<?php endif ?>
 		})(jQuery);
 	</script>
-<?php fs_require_template( 'powered-by.php' ) ?>
+<?php
+	$params = array(
+		'page'           => 'addons',
+		'module_id'      => $fs->get_id(),
+		'module_slug'    => $slug,
+		'module_version' => $fs->get_plugin_version(),
+	);
+	fs_require_template( 'powered-by.php', $params );
+?>
