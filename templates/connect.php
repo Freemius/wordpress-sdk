71540f557f78ab80c367cf7185e19614fb3a68b4
Leo Fajardo <leo@rating-widget.com>
2016-08-04 00:37:44 +0800

    [modal] [css] [cleanup]

--- templates/connect.php
+++ templates/connect.php
@@ -259,7 +259,6 @@
 			'slug' => $slug
 		);
 
-		fs_enqueue_local_style( 'fs_license_resend_modal', '/admin/license-resend-modal.css' );
 		fs_require_template( 'license-key-resend-modal.php', $vars );
 	}
 ?>
