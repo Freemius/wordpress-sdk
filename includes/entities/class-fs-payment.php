7a091a6150002765fba8aa7b583ac8e26b44179c
Vova Feldman <vova@freemius.com>
2016-08-16 21:34:21 -0400

    [minor] [php-doc]

--- includes/entities/class-fs-payment.php
+++ includes/entities/class-fs-payment.php
@@ -1,7 +1,7 @@
 <?php
 	/**
-	 * @package     Freemius for EDD Add-On
-	 * @copyright   Copyright (c) 2015, Freemius, Inc.
+	 * @package     Freemius
+	 * @copyright   Copyright (c) 2016, Freemius, Inc.
 	 * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
 	 * @since       1.0.0
 	 */
