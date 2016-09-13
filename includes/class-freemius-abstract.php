b2ee381f92fa423d234ea0588542985acea379ea
Vova Feldman <vova@freemius.com>
2016-09-12 21:16:38 -0400

    [module-type] Updated `is_freemium()`.

--- includes/class-freemius-abstract.php
+++ includes/class-freemius-abstract.php
@@ -340,8 +340,7 @@
 		 * @return bool
 		 */
 		function is_freemium() {
-			return ! $this->is_only_premium() &&
-			       $this->has_paid_plan() &&
+			return $this->has_paid_plan() &&
 			       $this->has_free_plan();
 		}
 
