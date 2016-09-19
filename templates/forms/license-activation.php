4c6580b61205953b397dfdb92b4722db78c2bde8
Leo Fajardo <leo@rating-widget.com>
2016-09-19 20:37:33 +0800

    [templates] [license-activation] [bug-fix] Was using "$this" instead of "$fs".

--- templates/forms/license-activation.php
+++ templates/forms/license-activation.php
@@ -19,9 +19,9 @@
 	if ($fs->is_registered()) {
 		// The URL to redirect to after successfully activating the license from the "Plugins" page.
 		if ( $fs->is_addon() ) {
-			$sync_license_url = $fs->get_parent_instance()->_get_sync_license_url( $this->_plugin->id, true );
+			$sync_license_url = $fs->get_parent_instance()->_get_sync_license_url( $fs->get_id(), true );
 		} else {
-			$sync_license_url = $fs->_get_sync_license_url( $this->_plugin->id, true );
+			$sync_license_url = $fs->_get_sync_license_url( $fs->get_id(), true );
 		}
 
 		/**
