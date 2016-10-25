f0bfa21b3f1c8b1dc71eb0fe9a89f53e38091e2a
Vova Feldman <vova@freemius.com>
2016-10-25 16:27:15 -0400

    [freemium] Adjusted `has_free_plan` to work even before opt-ing in.

--- includes/class-freemius.php
+++ includes/class-freemius.php
@@ -5427,9 +5427,7 @@
 		 * @return bool
 		 */
 		function has_free_plan() {
-			return ! $this->is_premium() ||
-			       ! $this->is_only_premium() ||
-			       FS_Plan_Manager::instance()->has_free_plan( $this->_plans );
+			return ! $this->is_only_premium();
 		}
 
 		/**
