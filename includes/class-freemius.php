8c266007915ade0b67bcf2feca2a3a64ccb6dad4
Vova Feldman <vova@freemius.com>
2016-11-05 18:49:01 -0400

    [debug] [rename] Renamed `add_debug_page()` to `_add_debug_section()` to pass Theme Check + the '_' prefix is there to warn devs it's a private method and shouldn't be used directly.

--- includes/class-freemius.php
+++ includes/class-freemius.php
@@ -1327,7 +1327,7 @@
 			// Configure which Freemius powered plugins should be auto updated.
 //			add_filter( 'auto_update_plugin', '_include_plugins_in_auto_update', 10, 2 );
 
-			add_action( 'admin_menu', array( 'Freemius', 'add_debug_page' ) );
+			add_action( 'admin_menu', array( 'Freemius', '_add_debug_section' ) );
 
 			add_action( "wp_ajax_fs_toggle_debug_mode", array( 'Freemius', '_toggle_debug_mode' ) );
 
@@ -1364,7 +1364,7 @@
 		 * @author Vova Feldman (@svovaf)
 		 * @since  1.0.8
 		 */
-		static function add_debug_page() {
+		static function _add_debug_section() {
 			if ( ! current_user_can( 'activate_plugins' ) ) {
 				return;
 			}
