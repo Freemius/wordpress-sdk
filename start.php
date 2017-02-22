051cd0f70c56ebe730e4eaaf7b9ee324c7c43438
Vova Feldman <vova.feldman@gmail.com>
2017-02-22 07:07:01 -0500

    [version]

--- start.php
+++ start.php
@@ -15,7 +15,7 @@
 	 *
 	 * @var string
 	 */
-	$this_sdk_version = '1.2.2';
+	$this_sdk_version = '1.2.2.1';
 
 	#region SDK Selection Logic --------------------------------------------------------------------
 
