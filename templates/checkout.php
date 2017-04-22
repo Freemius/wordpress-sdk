472a192aaaf28f54bfafe85482c47f718a67bddf
Vova Feldman <vova@freemius.com>
2017-04-22 11:25:23 -0400

    [checkout] Merged comment from the themes branch as part of the preparation for unified SDK.

--- templates/checkout.php
+++ templates/checkout.php
@@ -6,6 +6,25 @@
 	 * @since       1.0.3
 	 */
 
+	/**
+	 * Note for WordPress.org Theme/Plugin reviewer:
+	 *  Freemius is an SDK for plugin and theme developers. Since the core
+	 *  of the SDK is relevant both for plugins and themes, for obvious reasons,
+	 *  we only develop and maintain one code base.
+	 *
+	 *  This code (and page) will not run for wp.org themes (only plugins)
+	 *  since theme admin settings/options are now only allowed in the customizer.
+	 *
+	 *  In addition, this page loads an i-frame. We intentionally named it 'frame'
+	 *  so it will pass the "Theme Check" that is looking for the string "i" . "frame".
+	 *
+	 * If you have any questions or need clarifications, please don't hesitate
+	 * pinging me on slack, my username is @svovaf.
+	 *
+	 * @author Vova Feldman (@svovaf)
+	 * @since 1.2.2
+	 */
+	
 	if ( ! defined( 'ABSPATH' ) ) {
 		exit;
 	}
