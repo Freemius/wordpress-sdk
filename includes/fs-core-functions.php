570f2d3f988c0e0d5be421c7f64b2db1abe7042b
Vova Feldman <vova@freemius.com>
2017-12-19 09:25:56 +0200

    [core] [ajax] [action] [fix] Check if the request's action is string since other plugins can also use the 'action' argument to pass non-string values which may throw a warning in those cases.

--- includes/fs-core-functions.php
+++ includes/fs-core-functions.php
@@ -165,14 +165,14 @@
     }
 
     function fs_get_action( $action_key = 'action' ) {
-        if ( ! empty( $_REQUEST[ $action_key ] ) ) {
+            if ( ! empty( $_REQUEST[ $action_key ] ) && is_string( $_REQUEST[ $action_key ] ) ) {
             return strtolower( $_REQUEST[ $action_key ] );
         }
 
         if ( 'action' == $action_key ) {
             $action_key = 'fs_action';
 
-            if ( ! empty( $_REQUEST[ $action_key ] ) ) {
+                if ( ! empty( $_REQUEST[ $action_key ] ) && is_string( $_REQUEST[ $action_key ] ) ) {
                 return strtolower( $_REQUEST[ $action_key ] );
             }
         }
