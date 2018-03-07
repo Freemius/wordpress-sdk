392603ba9c08dc4b012db1c5fd30c008c77a5592
Vova Feldman <vova@freemius.com>
2018-03-07 19:07:39 +0200

    [ui] [pricing] Forgot to enqueue the common stylesheet on the pricing page.

--- templates/pricing.php
+++ templates/pricing.php
@@ -41,6 +41,7 @@
 	wp_enqueue_script( 'json2' );
 	fs_enqueue_local_script( 'postmessage', 'nojquery.ba-postmessage.min.js' );
 	fs_enqueue_local_script( 'fs-postmessage', 'postmessage.js' );
+	fs_enqueue_local_style( 'fs_common', '/admin/common.css' );
 
 	$fs        = freemius( $VARS['id'] );
 	$slug 	   = $fs->get_slug();
