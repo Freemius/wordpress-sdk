2c601e27004d9272186e5b2dee01228c52f8fb9f
Vova Feldman <vova@freemius.com>
2018-03-18 15:30:54 +0200

    [add-ons] Adjusted the pricing URL generation when the context module is an add-on to generate a direct link to the add-on's dialog in the add-ons page. Ref #219

--- includes/class-freemius.php
+++ includes/class-freemius.php
@@ -11130,6 +11130,10 @@
                 $params['trial'] = 'true';
             }
 
+            if ( $this->is_addon() ) {
+                return $this->_parent->addon_url( $this->_slug );
+            }
+
             return $this->_get_admin_page_url( 'pricing', $params );
         }
 
