10c3ada9217cef01bef02d817dfac593b8b9b031
Leo Fajardo <leo@freemius.com>
2018-05-15 01:23:48 +0800

    [eu] [gdpr] [opt-in] [admin-notice] [js] [enhancements]

--- templates/gdpr-optin-js.php
+++ templates/gdpr-optin-js.php
@@ -16,46 +16,51 @@
     $fs = freemius( $VARS['id'] );
 ?>
 <script type="text/javascript">
-	jQuery( document ).ready(function( $ ) {
-	    var $gdprOptinNotice = $( 'div[data-id^="gdpr_optin_actions"]' );
-	    if ( 0 === $gdprOptinNotice.length ) {
-	        return;
-        }
-
-        $gdprOptinNotice.on( 'click', '.button', function() {
-			var
-                allowMarketing = $( this ).hasClass( 'allow-marketing' ),
-                cursor         = $( this ).css( 'cursor' ),
-                $products      = $gdprOptinNotice.find( 'span[data-plugin-id]' ),
-                pluginIDs      = [];
-
-			if ( $products.length > 0 ) {
-			    $products.each(function() {
-			        pluginIDs.push( $( this ).data( 'plugin-id' ) );
-                });
+    (function( $ ) {
+        $( document ).ready(function() {
+            var $gdprOptinNotice = $( 'div[data-id^="gdpr_optin_actions"]' );
+            if ( 0 === $gdprOptinNotice.length ) {
+                return;
             }
 
-            $.ajax({
-                url       : ajaxurl + '?' + $.param({
-                    action   : '<?php echo $fs->get_ajax_action( 'gdpr_optin_action' ) ?>',
-                    security : '<?php echo $fs->get_ajax_security( 'gdpr_optin_action' ) ?>',
-                    module_id: '<?php echo $fs->get_id() ?>'
-                }),
-                method    : 'POST',
-                data      : {
-                    allow_marketing: allowMarketing,
-                    plugin_ids     : pluginIDs
-                },
-                beforeSend: function() {
-                    $gdprOptinNotice.find( '.button' ).addClass( 'disabled' );
-                    $( this ).css({'cursor': 'wait'});
-                },
-                complete  : function() {
-                    $( this ).css({'cursor': cursor});
+            $gdprOptinNotice.on( 'click', '.button', function() {
+                var
+                    $this          = $( this ),
+                    allowMarketing = $this.hasClass( 'allow-marketing' ),
+                    cursor         = $this.css( 'cursor' ),
+                    $products      = $gdprOptinNotice.find( 'span[data-plugin-id]' ),
+                    pluginIDs      = [];
 
-                    $gdprOptinNotice.remove();
+                if ( $products.length > 0 ) {
+                    $products.each(function() {
+                        pluginIDs.push( $( this ).data( 'plugin-id' ) );
+                    });
                 }
+
+                $.ajax({
+                    url       : ajaxurl + '?' + $.param({
+                        action   : '<?php echo $fs->get_ajax_action( 'gdpr_optin_action' ) ?>',
+                        security : '<?php echo $fs->get_ajax_security( 'gdpr_optin_action' ) ?>',
+                        module_id: '<?php echo $fs->get_id() ?>'
+                    }),
+                    method    : 'POST',
+                    data      : {
+                        allow_marketing: allowMarketing,
+                        plugin_ids     : pluginIDs
+                    },
+                    beforeSend: function() {
+                        $this.text( <?php fs_json_encode_echo_inline( 'Thanks, please wait', 'thanks-please-wait', $fs->get_slug() ) ?> + '...' );
+                        $this.css({'cursor': 'wait'});
+
+                        $gdprOptinNotice.find( '.button' ).addClass( 'disabled' );
+                    },
+                    complete  : function() {
+                        $this.css({'cursor': cursor});
+
+                        $gdprOptinNotice.remove();
+                    }
+                });
             });
-		});
-	});
+        });
+    })( jQuery );
 </script>
