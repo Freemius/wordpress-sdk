2a92c1282727272b2b945447e92440414fca6633
Leo Fajardo <leo@freemius.com>
2019-04-14 18:58:30 +0800

    [add-on] [checkout] Fixed the checkout link for add-ons.

--- start.php
+++ start.php
@@ -15,7 +15,7 @@
 	 *
 	 * @var string
 	 */
-	$this_sdk_version = '2.2.4.9';
+	$this_sdk_version = '2.2.5.2';
 
 	#region SDK Selection Logic --------------------------------------------------------------------
 
