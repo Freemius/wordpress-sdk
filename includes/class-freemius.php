e0144d6a4219d36b9c7bf141a7263d9f35e5c5f8
Vova Feldman <vova@freemius.com>
2019-05-24 10:59:41 +0300

    [license-migration] [fix] Activate migrated license network wide if the plugin is network activated and integrated, regardless if the context is the network or site-level WP admin.

--- includes/class-freemius.php
+++ includes/class-freemius.php
@@ -11931,6 +11931,11 @@
         }
 
         /**
+         * A helper method to activate migrated licenses. If the product is network activated and integrated, the method will network activate the license.
+         *
+         * @author Vova Feldman (@svovaf)
+         * @since  2.2.5
+         *         
          * @param string      $license_key
          * @param null|bool   $is_marketing_allowed
          * @param null|number $plugin_id
@@ -11950,7 +11955,7 @@
         ) {
             return $this->activate_license(
                 $license_key,
-                ( fs_is_network_admin() && $this->is_network_active() ) ?
+                $this->is_network_active() ?
                     $this->get_sites_for_network_level_optin() :
                     array(),
                 $is_marketing_allowed,
