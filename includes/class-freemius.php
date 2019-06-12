b5da57cf9e2a9ed7ecc07b5862279b9d03170447
Vova Feldman <vova@freemius.com>
2019-06-12 19:59:50 +0300

    [license-migration] [tweak] Remove the sticky after license activation notice when migrating a license programmatically.

--- includes/class-freemius.php
+++ includes/class-freemius.php
@@ -12169,7 +12169,7 @@
             $is_marketing_allowed = null,
             $plugin_id = null
         ) {
-            return $this->activate_license(
+            $result = $this->activate_license(
                 $license_key,
                 $this->is_network_active() ?
                     $this->get_sites_for_network_level_optin() :
@@ -12178,6 +12178,11 @@
                 null,
                 $plugin_id
             );
+
+            // No need to show the sticky after license activation notice after migrating a license.
+            $this->_admin_notices->remove_sticky( 'plan_upgraded' );
+
+            return $result;
         }
 
         /**
