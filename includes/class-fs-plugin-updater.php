685f56b9e231479ceef98d2f7f356de229508f7e
Leo Fajardo <leo@freemius.com>
2019-06-24 00:56:30 +0800

    [updater] Prevent previous update information from showing up on the updates page after the update process.

--- includes/class-fs-plugin-updater.php
+++ includes/class-fs-plugin-updater.php
@@ -487,13 +487,35 @@
                 return $transient_data;
             }
 
+            global $wp_current_filter;
+
+            $current_plugin_version = $this->_fs->get_plugin_version();
+
+            if ( ! empty( $wp_current_filter ) && 'upgrader_process_complete' === $wp_current_filter[0] ) {
+                if (
+                    is_null( $this->_update_details ) ||
+                    ( is_object( $this->_update_details ) && $this->_update_details->new_version !== $current_plugin_version )
+                ) {
+                    /**
+                     * After an update, clear the stored update details and reparse the plugin's main file in order to get
+                     * the updated version's information and prevent the previous update information from showing up on the
+                     * updates page.
+                     *
+                     * @author Leo Fajardo (@leorw)
+                     * @since 2.3.1
+                     */
+                    $this->_update_details  = null;
+                    $current_plugin_version = $this->_fs->get_plugin_version( true );
+                }
+            }
+
             if ( ! isset( $this->_update_details ) ) {
                 // Get plugin's newest update.
                 $new_version = $this->_fs->get_update(
                     false,
                     fs_request_get_bool( 'force-check' ),
                     WP_FS__TIME_24_HOURS_IN_SEC / 24,
-                    $this->_fs->get_plugin_version()
+                    $current_plugin_version
                 );
 
                 $this->_update_details = false;
