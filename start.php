4ef8ad8b6cb24f08a3307e9538b24dfce65308a6
Vova Feldman <vova@freemius.com>
2019-09-04 07:47:01 +0300

    [version-bump]

--- start.php
+++ start.php
@@ -15,7 +15,7 @@
 	 *
 	 * @var string
 	 */
-	$this_sdk_version = '2.3.0.3';
+	$this_sdk_version = '2.3.0.4';
 
 	#region SDK Selection Logic --------------------------------------------------------------------
 
