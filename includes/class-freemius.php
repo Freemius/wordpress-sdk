00b5b4c4e7b6f8821c05a20cc8c2d14c23f22899
Vova Feldman <vova@freemius.com>
2020-03-12 18:46:10 +0200

    [after-activation] [fix] Was redirecting to the plugin's account page instead of the after activation one.

--- includes/class-freemius.php
+++ includes/class-freemius.php
@@ -13221,7 +13221,7 @@
 
                     $next_page = $fs->is_addon() ?
                         $fs->get_parent_instance()->get_account_url() :
-                        $fs->get_account_url();
+                        $fs->get_after_activation_url();
                 }
             } else {
                 $next_page = $fs->opt_in(
