246159f6b9cc0b453e68fda6dadf990e131632ad
Leo Fajardo <leo@freemius.com>
2020-04-07 18:34:54 +0800

    [fs-version] The version update was not included in the previously merged PR (for the fix for the logic that determines a new plugin install).

--- start.php
+++ start.php
@@ -15,7 +15,7 @@
 	 *
 	 * @var string
 	 */
-	$this_sdk_version = '2.3.2.6';
+	$this_sdk_version = '2.3.2.7';
 
 	#region SDK Selection Logic --------------------------------------------------------------------
 
