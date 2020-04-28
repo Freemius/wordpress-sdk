926ead4e69ceb3539aaa2d042bbabe740193bcaf
Leo Fajardo <leo@freemius.com>
2020-04-28 18:45:19 +0800

    [ms] [menu] [fs-version]

--- start.php
+++ start.php
@@ -15,7 +15,7 @@
 	 *
 	 * @var string
 	 */
-	$this_sdk_version = '2.3.2.9';
+	$this_sdk_version = '2.3.2.10';
 
 	#region SDK Selection Logic --------------------------------------------------------------------
 
