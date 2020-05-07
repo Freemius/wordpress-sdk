d207df2da60a32e5d231f36da295629ed84857ef
Leo Fajardo <leo@freemius.com>
2020-05-08 01:25:49 +0800

    [fs-version] Forgot to include in the previously merged PR.

--- start.php
+++ start.php
@@ -15,7 +15,7 @@
 	 *
 	 * @var string
 	 */
-	$this_sdk_version = '2.3.2.10';
+	$this_sdk_version = '2.3.2.11';
 
 	#region SDK Selection Logic --------------------------------------------------------------------
 
