027d4191a93bb46dd65a8a195d4af84ff737316e
chen <xiaheng@freemius.com>
2020-10-15 23:55:04 +0800

    [debug] [minor] [update] Removed unnecessary comments.

--- templates/debug.php
+++ templates/debug.php
@@ -150,8 +150,6 @@
             if (!response.ok) {
                 alert("Error response code: " + response.status);
             } else {
-                // if HTTP-status is 200-299
-                // get the response body (the method explained below)
                 let json = await response.json();
 
                 alert("Response code:" + response.status + "\nResponse: " + JSON.stringify(json));
