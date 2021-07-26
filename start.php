a44da399308452b38d427f93da1702336cbe682a
Vova Feldman <vova@freemius.com>
2021-07-26 12:11:12 +0300

    [version]

--- start.php
+++ start.php
@@ -15,7 +15,7 @@
 	 *
 	 * @var string
 	 */
-	$this_sdk_version = '2.4.2.8';
+	$this_sdk_version = '2.4.2.9';
 
 	#region SDK Selection Logic --------------------------------------------------------------------
 
