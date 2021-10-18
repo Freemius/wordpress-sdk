9c57f00ed8f73f70e2102257c8ccc3d11154df11
Leo Fajardo <leo@freemius.com>
2021-10-18 23:20:07 +0800

    [license] [account] [fix] When checking if a license is foreign, ensure that there's a valid license object.

--- start.php
+++ start.php
@@ -15,7 +15,7 @@
 	 *
 	 * @var string
 	 */
-	$this_sdk_version = '2.4.2.16';
+	$this_sdk_version = '2.4.2.17';
 
 	#region SDK Selection Logic --------------------------------------------------------------------
 
