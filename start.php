17260c68f1630c461ac78f66ecc28df0e2c56151
Leo Fajardo <leo@freemius.com>
2021-11-02 20:37:36 +0800

    [filters] Fixed the usage of apply_filters().

--- start.php
+++ start.php
@@ -15,7 +15,7 @@
 	 *
 	 * @var string
 	 */
-	$this_sdk_version = '2.4.2.17';
+	$this_sdk_version = '2.4.2.18';
 
 	#region SDK Selection Logic --------------------------------------------------------------------
 
