94cfb40762c832fc572ea4eb8cd0d503ad2a721e
Malay Ladu <malay@freemius.com>
2021-12-28 19:01:06 +0530

    [comment] Added comment.

--- includes/class-freemius.php
+++ includes/class-freemius.php
@@ -20980,7 +20980,7 @@
                         $this->switch_to_blog( $this->_storage->network_install_blog_id );
                     }
 
-                    // Show API messages only if paying customer.
+	                // Show API message only if not background sync or if paying customer. And if it's a background process, show message only if it's a paying customer.
                     if ( ! $background || $this->is_paying() ) {
                         // Try to ping API to see if not blocked.
                         if ( ! FS_Api::test() ) {
@@ -21026,7 +21026,7 @@
 		                        );
 	                        }
                         } else {
-                            // API is working now. Clear transient to start afresh.
+                            // API is working now. Delete the transient and start afresh.
                             delete_transient('_fs_api_connection_retry_counter');
 
                             // Authentication params are broken.
