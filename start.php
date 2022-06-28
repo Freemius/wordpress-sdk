0761162d7586e40b447cd095b5944a2c2f28d253
Leo Fajardo <leo@freemius.com>
2022-06-28 23:19:13 +0800

    [clone-resolution] [fs-version]

--- start.php
+++ start.php
@@ -15,7 +15,7 @@
 	 *
 	 * @var string
 	 */
-	$this_sdk_version = '2.5.0.9';
+	$this_sdk_version = '2.5.0.11';
 
 	#region SDK Selection Logic --------------------------------------------------------------------
 
