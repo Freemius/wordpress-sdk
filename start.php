9eba0608ed5c6eb549d95701c784f583998ac5be
Leo Fajardo <leo@freemius.com>
2022-07-04 19:57:46 +0800

    [fs] [version] Minor version update.

--- start.php
+++ start.php
@@ -15,7 +15,7 @@
 	 *
 	 * @var string
 	 */
-	$this_sdk_version = '2.5.0.10';
+	$this_sdk_version = '2.5.0.11';
 
 	#region SDK Selection Logic --------------------------------------------------------------------
 
