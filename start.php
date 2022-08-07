da8b1fe464bebf3863836e470ec56d24c6a69994
Leo Fajardo <leo@freemius.com>
2022-08-07 16:01:53 +0800

    [deactivation] [snoozing] Store the snooze period only if the user has activated the snoozing option on the deactivation feedback dialog.

--- start.php
+++ start.php
@@ -15,7 +15,7 @@
 	 *
 	 * @var string
 	 */
-	$this_sdk_version = '2.5.0.12';
+	$this_sdk_version = '2.5.0.13';
 
 	#region SDK Selection Logic --------------------------------------------------------------------
 
