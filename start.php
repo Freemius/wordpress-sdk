f650715211135ffa2354022e277dba95b0d10998
Leo Fajardo <leo@freemius.com>
2022-09-29 00:01:31 +0800

    [activation] Clear the pending activation mode when the premium version is activated.

--- start.php
+++ start.php
@@ -15,7 +15,7 @@
 	 *
 	 * @var string
 	 */
-	$this_sdk_version = '2.5.0.16';
+	$this_sdk_version = '2.5.0.17';
 
 	#region SDK Selection Logic --------------------------------------------------------------------
 
