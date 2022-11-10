e3bc6a75aebe5d11d1ff763de17429452cf50945
Leo Fajardo <leo@freemius.com>
2022-11-10 15:14:33 +0800

    [clone-resolution] [lock] Prefix the ID of the lock that is used by the clone resolution mechanism with our prefix that is defined in the config.

--- includes/managers/class-fs-clone-manager.php
+++ includes/managers/class-fs-clone-manager.php
@@ -647,7 +647,7 @@
                 return;
             }
 
-            $lock = new FS_Lock( self::OPTION_NAME . '_subsite' );
+            $lock = new FS_Lock( WP_FS___OPTION_PREFIX . self::OPTION_NAME . '_subsite' );
 
             if ( ! $lock->try_lock(60) ) {
                 return;
@@ -762,7 +762,7 @@
 
             require_once WP_FS__DIR_INCLUDES . '/class-fs-lock.php';
 
-            $lock = new FS_Lock( self::OPTION_NAME );
+            $lock = new FS_Lock( WP_FS___OPTION_PREFIX . self::OPTION_NAME );
 
             /**
              * Try to acquire lock for the next 60 sec based on the thread ID.
