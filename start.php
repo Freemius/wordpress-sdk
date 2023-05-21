7117bdeddaeee01ac03df341b709e385869998d1
Leo Fajardo <leo@freemius.com>
2023-05-21 20:52:34 +0800

    [admin-notice] [opt-in] [gdpr] GDPR opt-in notice URL update.

--- start.php
+++ start.php
@@ -15,7 +15,7 @@
 	 *
 	 * @var string
 	 */
-	$this_sdk_version = '2.5.8.1';
+	$this_sdk_version = '2.5.8.2';
 
 	#region SDK Selection Logic --------------------------------------------------------------------
 
