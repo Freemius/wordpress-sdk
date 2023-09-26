b609a971be6f38f6bd53a474b5cb4014c3e35a16
Swashata Ghosh <swas@freemius.com>
2023-09-26 17:01:00 +0530

    [version] [bump] 2.5.12.7

--- start.php
+++ start.php
@@ -15,7 +15,7 @@
 	 *
 	 * @var string
 	 */
-	$this_sdk_version = '2.5.12.6';
+	$this_sdk_version = '2.5.12.7';
 
 	#region SDK Selection Logic --------------------------------------------------------------------
 
