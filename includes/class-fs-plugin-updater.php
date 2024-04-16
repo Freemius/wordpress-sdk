3c5a82a8bbead167e2d21e034b58c091f14d80c8
Laurence Bahiirwa <laurence@freemius.com>
2024-04-16 19:13:36 +0300

    [static-analysis] Fix php errors from static analysis.

--- includes/class-fs-plugin-updater.php
+++ includes/class-fs-plugin-updater.php
@@ -1094,7 +1094,7 @@
                         false
                     );
 
-                    if ( is_array( $addon_plugin_data ) && $addon_plugin_data['Version'] ) {
+                    if ( is_array( $addon_plugin_data ) && isset( $addon_plugin_data['Version'] ) ) {
                         $addon_version = $addon_plugin_data['Version'];
                     }
                 }
