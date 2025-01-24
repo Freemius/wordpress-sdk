936609fe0ca249e5c057d2154ab67f2024de6e1c
Daniele Alessandra <danielealessandra@gmail.com>
2025-01-24 19:06:20 +0100

    Version number increase

--- start.php
+++ start.php
@@ -15,7 +15,7 @@
 	 *
 	 * @var string
 	 */
-	$this_sdk_version = '2.11.0.6';
+	$this_sdk_version = '2.11.0.7';
 
 	#region SDK Selection Logic --------------------------------------------------------------------
 
