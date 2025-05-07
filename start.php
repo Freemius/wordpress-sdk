49b0e6a2a136eeb4bd9163d28097dd8029abfccf
Leo Fajardo <leo@freemius.com>
2025-05-07 19:45:13 +0800

    [license-activation] [opt-in] License activation opt-in params fix. (#801)

--- start.php
+++ start.php
@@ -15,7 +15,7 @@
 	 *
 	 * @var string
 	 */
-	$this_sdk_version = '2.11.0.9';
+	$this_sdk_version = '2.11.0.10';
 
 	#region SDK Selection Logic --------------------------------------------------------------------
 
