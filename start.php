ff6f960fdb7cd0e3549239c03c6ebf0422c92e14
Daniele Alessandra <daniele@freemius.com>
2026-06-16 15:03:56 +0200

    [feature] Add additional currency symbols to pricing page

--- start.php
+++ start.php
@@ -15,7 +15,7 @@
 	 *
 	 * @var string
 	 */
-	$this_sdk_version = '2.13.2';
+	$this_sdk_version = '2.13.2.1';
 
 	#region SDK Selection Logic --------------------------------------------------------------------
 
